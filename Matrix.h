f8bfc0b207f23f8a81033a68f94d531da2b91454
marco <marco.benelli@stud.unifi.it>
2018-08-18 13:48:59 +0200

    Basic operations, no factory

---
+++ Matrix.h
@@ -0,0 +1,184 @@
+//
+// Created by marco on 21/07/18.
+//
+
+#ifndef MATRICI_MATRIX_H
+#define MATRICI_MATRIX_H
+
+
+#include <iostream>
+
+template<typename T>
+class Matrix {
+public:
+    Matrix(int m, int n) : m(m), n(n), buffer(new T[m * n]) {}
+
+    virtual ~Matrix() {
+        delete[] buffer;
+    }
+
+    T &element(int i, int j) const {
+        return buffer[i * n + j];
+    }
+
+    void print() const;
+
+    Matrix(const Matrix &original);
+
+    Matrix<T> &operator=(const Matrix &right);
+
+    int getM() const {
+        return m;
+    }
+
+    int getN() const {
+        return n;
+    }
+
+    bool operator==(const Matrix &right) const;
+
+    bool operator!=(const Matrix &right) const {
+        return !(*this == right);
+    }
+
+    Matrix<T> operator+(const Matrix &right) const;
+
+    Matrix<T> operator+=(const Matrix &right) {
+        return *this = *this + right;
+    }
+
+    Matrix<T> operator*(T scalar) const;
+
+    Matrix<T> operator*=(T scalar) {
+        return *this = *this * scalar;
+    }
+
+    Matrix<T> operator-() const;
+
+    Matrix<T> operator-(const Matrix &right) const {
+        return *this + -right;
+    }
+
+    Matrix<T> operator-=(const Matrix &right) {
+        return *this = *this - right;
+    }
+
+    Matrix<T> operator*(const Matrix &right) const;
+
+    Matrix<T> operator*=(const Matrix &right) {
+        return *this = *this * right;
+    }
+
+    Matrix<T> slice(int imin, int jmin, int imax, int jmax) const;
+
+    Matrix<T> trasposta() const;
+
+private:
+    int m, n;
+    T *buffer;
+
+    void copy(const Matrix &that);
+};
+
+template<typename T>
+void Matrix<T>::print() const {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j)
+            std::cout << element(i, j) << " ";
+        std::cout << std::endl;
+    }
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix &original) : Matrix(original.m, original.n) {
+    copy(original);
+}
+
+template<typename T>
+Matrix<T> &Matrix<T>::operator=(const Matrix &right) {
+    delete[] buffer;
+    m = right.m;
+    n = right.n;
+    buffer = new T(m * n);
+    copy(right);
+    return *this;
+}
+
+template<typename T>
+void Matrix<T>::copy(const Matrix &that) {
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            element(i, j) = that.element(i, j);
+}
+
+template<typename T>
+bool Matrix<T>::operator==(const Matrix &right) const {
+    if (m != right.m || n != right.n)
+        return false;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            if (element(i, j) != right.element(i, j))
+                return false;
+    return true;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix &right) const {
+    Matrix ans(m, n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            ans.element(i, j) = element(i, j) + right.element(i, j);
+    return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(T scalar) const {
+    Matrix ans(m, n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            ans.element(i, j) = scalar * element(i, j);
+    return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix &right) const {
+    Matrix ans(m, right.n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < right.n; ++j) {
+            ans.element(i, j) = 0;
+            for (int k = 0; k < n; ++k)
+                ans.element(i, j) += element(i, k) * right.element(k, j);
+        }
+    return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-() const {
+    Matrix ans(m, n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            ans.element(i, j) = -element(i, j);
+    return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::slice(int imin, int jmin, int imax, int jmax) const {
+    //i min sono compresi, i max no
+    Matrix ans(imax - imin, jmax - jmin);
+    for (int i = 0; i < ans.m; ++i)
+        for (int j = 0; j < ans.n; ++j)
+            ans.element(i, j) = element(imin + i, jmin + j);
+    return ans;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::trasposta() const {
+    Matrix ans(n, m);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            ans.element(j, i) = element(i, j);
+    return ans;
+}
+
+
+#endif //MATRICI_MATRIX_H
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "Matrix.h"
+
+int main() {
+    int m1, n1;
+    m1 = 2;
+    n1 = 3;
+    Matrix<int> M1(m1, n1);
+    for (int i = 0; i < m1; ++i)
+        for (int j = 0; j < n1; ++j)
+            M1.element(i, j) = 1;
+    Matrix<int> M2(m1, n1);
+    for (int i = 0; i < m1; ++i)
+        for (int j = 0; j < n1; ++j)
+            M2.element(i, j) = 2;
+    M1.print();
+    M2.print();
+    Matrix<int> M3(M1);
+    M3.print();
+    M2 = M1;
+    M2.print();
+    M3 = M1 + M2;
+    std::cout << std::endl;
+    M3.print();
+    (M1 * 2).print();
+    std::cout << std::endl;
+    std::cout << "prodotto" << std::endl;
+    int m2, n2;
+    m2 = 3;
+    n2 = 2;
+    Matrix<int> M4(m2, n2);
+    M4.element(0, 0) = 1;
+    M4.element(0, 1) = 2;
+    M4.element(1, 0) = 3;
+    M4.element(1, 1) = 4;
+    M4.element(2, 0) = 5;
+    M4.element(2, 1) = 6;
+    M1.print();
+    std::cout << std::endl;
+    M4.print();
+    std::cout << std::endl;
+    auto M5 = M4 * M1;
+    M5.print();
+    std::cout << std::endl << "trasposta" << std::endl;
+    M4.trasposta().print();
+    std::cout << std::endl << "meno" << std::endl;
+    M1 = -M4;
+    M4.print();
+    M1.print();
+    (M4 - M1).print();
+    M1 += M4;
+    M1.print();
+    return 0;
+}
